8829001c6642aca2f09493a800bd2c065de745f7
OlViks <salova.40@yandex.ru>
2022-10-01 20:45:46 +0300

    сделал второе задание

--- src/Line.java
+++ src/Line.java
@@ -2,6 +2,7 @@ public class Line {
     public static void main(java.lang.String[] args) {
         System.out.println("Строки!");
         talk1_1();
+        talk1_2();
     }
 
     public static void talk1_1() {
@@ -11,6 +12,14 @@ public class Line {
         String lastName   = "Иванович" + " ";
         String fullName = firstName  + middleName + lastName;
         System.out.println("ФИО сотрудника — " + fullName);
-
+    }
+    public static void talk1_2() {
+        System.out.println("Задание №1");
+        String firstName = "Иванов" + " ";
+        String middleName = "Иван" + " ";
+        String lastName = "Иванович" + " ";
+        String fullName = firstName + middleName + lastName;
+        String fullNameUpper = fullName.toUpperCase();
+        System.out.println("ФИО сотрудника — " + fullNameUpper);
     }
 }
