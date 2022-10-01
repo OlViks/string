a04ba52c7e20b332e4b14bb9eb3ae2fb40600e15
OlViks <salova.40@yandex.ru>
2022-10-01 20:41:53 +0300

    сделал первое задание

--- src/Line.java
+++ src/Line.java
@@ -1,5 +1,16 @@
 public class Line {
     public static void main(java.lang.String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Строки!");
+        talk1_1();
+    }
+
+    public static void talk1_1() {
+        System.out.println("Задание №1");
+        String firstName = "Иванов" + " ";
+        String middleName  = "Иван" + " ";
+        String lastName   = "Иванович" + " ";
+        String fullName = firstName  + middleName + lastName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
     }
 }
